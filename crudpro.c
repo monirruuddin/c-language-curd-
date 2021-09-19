cce2371b980f3016761225ba347104c02d70a268
Monir Uddin <monirkm74@gmail.com>
2021-09-20 00:35:45 +0600

    done the project

---
+++ crudpro.c
@@ -0,0 +1,118 @@
+#include <stdio.h>
+#include <stdlib.h>
+int arr[20]= {1,2,3,4,5,6,7,8,9,10};
+
+void fullarrayread(){
+    int i;
+    for (i = 0; i <10; i++){
+        printf("%d,",arr[i]);
+    }
+    printf("\n");
+    printf("\n");
+    
+}
+int searching(int searchnumr){
+    int i;
+    for (i = 0; i <10; i++){
+        if (arr[i]==searchnumr){
+           printf("%d is found\n",arr[i]);
+           return 1;
+        }
+    }
+   printf("%d is Not found\n",searchnumr);
+   printf("\n");
+   printf("\n");   
+}
+void unumber(int uindex, int unum){
+    int i;
+    for (i = 0; i <10; i++){
+        if (i==uindex){
+            arr[uindex]=unum;
+           printf("Update %d\n",arr[i]);
+        }
+    }
+}
+
+int main(){
+
+    do
+    {
+        printf("input 1 to Create\n");
+        printf("input 2 to Read\n");
+        printf("input 3 to Update\n");
+        printf("input 4 to delete\n");
+        printf("input 5 to Exit The Program\n");
+        int choice;
+        scanf("%d",&choice);
+        if(choice==1){
+            printf("Create\n");
+        }else if(choice==2){
+            printf("press 1 to read full array\n");
+            printf("press 2 to read index of an array\n");
+            printf("press 3 to search index a number from array\n");
+
+
+            int readnumber;
+            scanf("%d",&readnumber);
+            if (readnumber==1){
+                fullarrayread();
+            }else if (readnumber==2){
+                int rindex;
+                printf("Put Index Number wihtin 1-10:");
+                
+                while(1){
+                    scanf("%d",&rindex);
+                    if( 0<rindex && 10>=rindex){
+                         printf("Index Number of a array: %d",arr[rindex]-1);
+                         break;
+                    }else{
+                     printf("Index Must be within 1-10:");
+                    }
+                }
+                
+
+                printf("\n");
+                printf("\n");
+            }else if(readnumber==3){
+                int searchnumr;
+                printf("Input your searching Number:");
+                scanf("%d",&searchnumr);
+                searching(searchnumr);      
+            }
+            
+        }else if(choice==3){
+            int uindex,unum;
+            printf("Here The full array\n");
+            fullarrayread();
+            printf("Do you want to update any number?\t just input index number\n");
+            scanf("%d",&uindex);
+            printf(" just type... update Number\n");
+            scanf("%d",&unum);
+            unumber(uindex,unum);
+
+        }else if(choice==4){
+            int dposition,i;
+            printf("Input the position which one do you want to delete\n");
+            scanf("%d",&dposition);
+            for (i = dposition-1; i <10; i++){
+                arr[i]=arr[i+1];
+            }
+            for (i = 0; i <10; i++){
+                printf("%d ",arr[i]);
+            }
+            printf("\n");
+            
+        }else if(choice==5){
+            exit(0);
+        }else{
+            printf("Press 1-5 Number:\n");
+            printf("\n");
+        }
+            
+        
+
+    } while (1);
+    
+  
+    return 0;
+}
